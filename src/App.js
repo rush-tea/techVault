fd2d35d342ff694149c0ea7b0a3866c36e998d34
rush-tea <tripathiadarsh39@gmail.com>
2021-02-20 22:36:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/mainContent.css';
+import MainContent from './components/MainContent';
+import RightTags from './components/Tags';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="headerContainer">
+        <div className="heading">
+          <span>
+            TechVault
+          </span>
+        </div>
+        <div className="headerLinks">
+          <a href="#">Contact Us</a>
+          <a href="#"> Log In/Register</a>
+        </div>
       </header>
+      <nav>
+        <div className="navLinks">
+          <a href="#">Latest</a>
+          <a href="#">Top Liked</a>
+          <a href="#">Top Viewed</a>
+          <select name="All Types" id="allTypes">
+            <option value="alltypes">All Types</option>
+          </select>
+        </div>
+        <div className="navSearch">
+          <input type="text" name="search" id="search" placeholder="Search..." />
+        </div>
+      </nav>
+      <main>
+        <div className="left-container">
+          <div className="mainContent left-tab">
+            <MainContent />
+           </div> 
+          <div className="left-tab"></div>
+          <div className="left-tab"></div>
+        </div>
+        <div className="right-container">
+          <RightTags />
+        </div>
+      </main>
     </div>
   );
 }
